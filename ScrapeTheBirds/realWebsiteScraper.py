61dee982f465831a8ec35fcb2cec4a175aadb7c8
Jessica Read <45640543+jessica-read@users.noreply.github.com>
2025-07-02 21:55:10 +0100

    020725 progress1 with assistance
    can now only search through date blocks, and records accurate dates, locations, and species with associated rarity

--- ScrapeTheBirds/realWebsiteScraper.py
+++ ScrapeTheBirds/realWebsiteScraper.py
@@ -2,17 +2,64 @@ from bs4 import BeautifulSoup
 import requests
 
 def parse_bird_sightings(notts_birders_content):
-    # print(f'Oh dear... here we go: \n {notts_birders_content}')
-    bird_soup = BeautifulSoup(notts_birders_content, 'lxml') #parses the entire website through BS
-    bird_species = bird_soup.find('span', class_ = 'style6')
+    bird_soup = BeautifulSoup(notts_birders_content, 'lxml')
+    sightings = []
 
-    print(bird_species) #JESS!!! SO FAR IT FINDS THE FIRST STLYE6 SPAN, NOT QUITE WHAT I WANTED BUT WE MOVE
-    returning_data = bird_species
-    return returning_data
+    # iterates through h2 tags holding the dates
+    for h2 in bird_soup.find_all('h2', class_='main_content_title'):
+        date = h2.get_text(strip=True)
+        current = h2.find_next_sibling()
 
-# <p><span class="style19"><strong>Holme Pierrepont</strong></span> -	
-#   <span class="style6">Cattle Egret</span> [circled Blotts Pit and flew west, 18:17]; also <span class="style6">Common Sandpiper</span>. 
-# </p>
+        # while the next line is not h2
+        while current and not (current.name == 'h2' and 'main_content_title' in current.get('class', [])):
+            if current.name == 'p':
+                # Identify location from span.style19 > strong
+                location = None
+                location_span = current.find('span', class_='style19')
+                if location_span:
+                    strong_in_span = location_span.find('strong')
+                    if strong_in_span:
+                        location = strong_in_span.text.strip()
+
+                if not location:
+                    current = current.find_next_sibling()
+                    continue
+
+                bird_entries = []
+
+                for element in current.find_all(['span', 'strong']):
+                    # Skip the location strong tag
+                    if element.name == 'strong' and element in location_span.find_all('strong'):
+                        continue
+
+                    bird_name = element.get_text(strip=True)
+
+                    if element.name == 'strong':
+                        rarity = 'unconfirmed'
+                    elif 'style6' in element.get('class', []):
+                        rarity = 'interesting'
+                    elif 'style47' in element.get('class', []):
+                        rarity = 'rare'
+                    elif 'style96' in element.get('class', []):
+                        rarity = 'very rare'
+                    else:
+                        continue
+
+                    bird_entries.append({'name': bird_name, 'rarity': rarity})
+
+                if bird_entries:
+                    sightings.append({
+                        'date': date,
+                        'location': location,
+                        'birds': bird_entries
+                    })
+
+            current = current.find_next_sibling()
+
+    return sightings
+
+
+
 
 # Fetch HTML from the website
 url = "https://www.nottsbirders.net/latest_sightings.html"
@@ -21,6 +68,6 @@ response = requests.get(url)
 if response.status_code == 200:
     notts_birders_content = response.text #content from the notts birders website
     organised_data = parse_bird_sightings(notts_birders_content)
-    # print(organised_data)
+    print(organised_data)
 else:
     print("Failed to retrieve page:", response.status_code)
